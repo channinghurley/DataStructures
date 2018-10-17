a136c49cb2055e5d0ea9f442b6c2dfd395019e47
channinghurley <chhu2413@colorado.edu>
2018-10-17 12:07:46 -0600

    Add parent tracking functionality for nodes and leaves

--- Trees/BinaryTree.py
+++ Trees/BinaryTree.py
@@ -13,41 +13,64 @@
 
 
 class BinaryTree:
-    """Top-level class to serve as parent of BinaryTree and Leaf to maintain the logic that a node consists of data, a left child, and a right child, Leaf is a tree with no data and no children, and that all subtrees within a tree can be themselves considered a tree. This design allows for convenience when implementing recursive methods to operate on trees. This class performs all top-level operations on the overall tree, such as insertion, deletion, and searching.
+    """Top-level class to serve as parent of BinaryTree and Leaf to maintain the logic that a node consists of data, a left child, and a right child, Leaf is a tree with no data and no children, and that all subtrees within a tree can be themselves considered a tree. This design allows for convenience when implementing recursive methods to operate on trees. This class performs all top-level operations on the overall tree, such as insertion, deletion, and searching and implements members common to both nodes and leaves.
+
+    Notes:
+        * References to any data, left children, or right children must be in refernece to a specific node, not the entire tree.
     """
 
     def __init__(self, root=None):
-        """Initialize the tree to be empty by default. The tree can also be initialized to a node with data and children. Note: if root is initialized to a node, the root node extends the BinaryTree class.
+        """Initialize the tree to be empty by default. The tree can also be initialized to a node with data and children. Note: if root is initialized to a node, the root node necessarily extends the BinaryTree class.
         """
         assert root is None or isinstance(root, BinaryTree)
+
         self.root = root
+        self.has_parent = False
 
     def __str__(self):
         """TODO: recursively print all nodes/leaves"""
         return str(self.root)
+
+    def set_parent(self, parent):
+        """Assign a refernce to the root node's parent"""
+        self.has_parent = True
+        self.parent = parent
+
+    def isright(self):
+        """Return True iff the tree is a right child. Note: parent must be set"""
+        return self is self.parent.right if self.has_parent else False
+
+    def isleft(self):
+        """Return True iff the tree is a left child. Note: parent must be set"""
+        return not self.isright() if self.has_parent else False
 
 
 class Leaf(BinaryTree):
     """"An empty node in a binary tree, i.e. a node with no data and no children ("leaf" node). Extends BinaryTree to allow for convenience when implementing recursive methods that operate on Trees and maintain the logic that all subtrees within a tree can be themselves considered a tree.
     """
 
+    def __init__(self):
+        BinaryTree.__init__(self) # Do not set root; leaves are not roots
+
     def __str__(self):
         return "Leaf"
 
 
 class Node(BinaryTree):
-    """A node in a binary tree containing the node data, a right child, and a left child. Extends BinaryTree to allow for convenience when implementing recursive methods that operate on Trees and to maintain the logic that all subtrees within a tree can be themselves considered a tree.
+    """A node in a binary tree containing the node data, a right child, a left child, and an optional "pointer" to the parent node. Extends BinaryTree to allow for convenience when implementing recursive methods that operate on Trees and to maintain the logic that all subtrees within a tree can be themselves considered a tree.
     """
 
     def __init__(
         self,
-        left: BinaryTree=Leaf(),
-        data=None,
-        right: BinaryTree=Leaf()
+        left: BinaryTree = Leaf(),
+        data = None,
+        right: BinaryTree = Leaf()
     ):
+        # Initialize base class and assign the root of this tree (or subtree) to be the current node
+        BinaryTree.__init__(self, root=self)
 
-        assert isinstance(left, BinaryTree)
-        assert isinstance(right, BinaryTree)
+        assert isinstance(left, BinaryTree), "Left child must be Node or Leaf"
+        assert isinstance(right, BinaryTree), "Right child must be Node or Leaf"
 
         self.left = left
         self.data = data
@@ -55,3 +78,11 @@ class Node(BinaryTree):
 
     def __str__(self):
         return "Node({}, {}, {})".format(self.left, self.data, self.right)
+
+    def add_right(self, r):
+        """Add the node's right child, overridding any existing right children"""
+        self.right = r
+
+    def add_left(self, l):
+        """Add the node's left child, overridding any existing left children"""
+        self.left = l
--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+"""Test file for tinkering"""
+
 from Trees.BinaryTree import *
 
 # t = Node(1)
@@ -8,21 +10,43 @@ from Trees.BinaryTree import *
 # t = BinaryTree(root=Node(1, Leaf(), Leaf()))
 # print(t)
 
-n4 = Node(Leaf(), 4, Leaf())
-n3 = Node(n4, 3, n4)
-n2 = Node(n3, 2, n3)
-r = Node(n2, 1, n2)
+# n4 = Node(Leaf(), 4, Leaf())
+# n3 = Node(n4, 3, Leaf())
+# n2 = Node(n3, 2, Leaf())
+# r = Node(n2, 1, Leaf())
+# n4.parent = n3
+# n3.parent = n2
+# n2.parent = r
 
 # print(r)
 
-ptr = r
-
-def pretty_print(n): # FIXME
+# TODO: dont print "|" after right children, may repuire ptr to parent to check if right child
+def pretty_print(n):
     def helper(n, depth=0):
         if not isinstance(n, Leaf):
-            print("   " * depth, n.data)
-            helper(n.left, depth + 1)
-            helper(n.right, depth + 1)
+            for i in range(depth - 1):
+                print("|  ", end='')
+            print("+--" if depth else "", n.root.data, sep='')
+            helper(n.root.left, depth + 1)
+            # print("|  " * depth)
+            helper(n.root.right, depth + 1)
     helper(n)
 
-pretty_print(r)
+
+tree = BinaryTree(root=Node())
+root = tree.root
+root.data = 2
+
+left = Node(data=1)
+right = Node(data=3)
+root.add_left(left)
+root.add_right(right)
+left.set_parent(root)
+right.set_parent(root)
+
+print(tree)
+print(root.isright())
+print(root.isleft())
+print(left.isleft())
+print(right.isright())
+pretty_print(tree)
