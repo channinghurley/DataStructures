6567e459c5a284a399521f5cfc967196ee03f54a
Channing Hurley <chhu2413@colorado.edu>
2019-01-23 19:45:48 -0700

    new file

--- sandbox.py
+++ sandbox.py
@@ -1,13 +1,44 @@
 """File for tinkering"""
 
-from Trees.BinaryTree import Node
-from Trees.Util import *
-from Lists.Util import fold as list_fold
+# from Trees.BinaryTree import Node
+# from Trees.Util import *
+# from Lists.Util import fold as list_fold
+from trees.binary_tree import *
+from trees.util import bfs
 
-test = Node(data=1, left=Node(data=1))
+t = Node(Node(data=2), 1, Node(data=3))
+n = Node(data=5)
+
+# print(t)
+#
+# t1 = Node()
+# t2 = Node()
+# print(t1 == t2)
+
 tree = Node(Node(Node(data="A"), "B", Node(Node(data="C"), "D", Node(data="E"))), "F", Node(None, "G", Node(Node(data="H"), "I")))
 
-tree2 = Node(Node(Node(data=4), 2, Node(data=5)), 1, Node(Node(data=6), 3, Node(data=7)))
+
+n.set_left(tree)
+
+bfs(tree, lambda n: print(n.data))
+
+
+# t2 = Node(Node(None, None, None), 1, Node(None, None, None))
+# print(t2)
+
+# l = [1,2,3,4,5]
+
+# print(l*2)
+
+# print(list(map(lambda x: print(x), l)))
+
+
+
+#
+# test = Node(data=1, left=Node(data=1))
+# tree = Node(Node(Node(data="A"), "B", Node(Node(data="C"), "D", Node(data="E"))), "F", Node(None, "G", Node(Node(data="H"), "I")))
+#
+# tree2 = Node(Node(Node(data=4), 2, Node(data=5)), 1, Node(Node(data=6), 3, Node(data=7)))
 
 # total = 0
 # def sum(n):
@@ -21,13 +52,13 @@ tree2 = Node(Node(Node(data=4), 2, Node(data=5)), 1, Node(Node(data=6), 3, Node(
 # print(fold(tree2, 0, lambda acc, n: acc + n.data))
 
 # bfs(tree2, lambda n: print(n.data))
-def inc(n):
-    n.data += 1
+# def inc(n):
+#     n.data += 1
 # bfs(tree2, inc)
 # bfs(tree2, lambda n: print(n.data))
 
 # print(max_depth(tree))
-print(tree.size())
+# print(tree.size())
 
 # def pretty_print(t): # TODO: store last_depth in data, use bfs w/process.
     # n = t
