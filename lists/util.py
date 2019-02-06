5751cc257281d269a0aa0973b4421ea8b8c1fcf1
Channing Hurley <chhu2413@colorado.edu>
2019-02-06 16:27:19 -0700

    fold docs

--- lists/util.py
+++ lists/util.py
@@ -8,7 +8,7 @@ def descending(l):
     return ascending(list(reversed(l)))
 
 def fold(l, acc, op):
-    """Inspired by Scala's List.fold function, fold will recursively "fold" a list by applying operation "op" to the accumulator "acc" and each element of the list. Operation "op" should be a callable that will return the new accumulator. The result of op will become the new accumulator value for the next recursive step.
+    """Inspired by Scala's List.fold function, fold will recursively "fold" a list by applying operation "op" to the accumulator "acc" and each element of the list. Operation "op" should be a callable that will return the new accumulator. The result of op will become the new accumulator value for the next recursive step. Similar to functools.reduce, however the caller can maipulate the accumulator.
     """
 
     assert callable(op), "Operation must be callable."
